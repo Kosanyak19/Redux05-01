c7214d3f19c8684e876896b55baefd49a9f2719d
Kosanyak19 <kosvik264@gmail.com>
2021-02-25 23:25:17 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Users from './users/Users';
 import store from './store'
+import UsersList from './users/UsersList';
 
 const App = () => (
     <Provider store={store}>
-      <Users />
+      <UsersList />
     </Provider>
   );
 
